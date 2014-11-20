8ebaff3ff00f3a1f05b8639d2934d79cec1f6811
woostar <00b0_0b00@naver.com>
2014-11-21 00:35:02 +0100

    real02
    realreal

---
+++ index.go
@@ -0,0 +1,18 @@
+﻿package main
+ 
+import (
+    "net/http"
+    "text/template"
+    "html"
+)
+ 
+var mainTemplate, _ = template.ParseFiles("topics.html")
+ 
+func mainPage(w http.ResponseWriter, r *http.Request) {
+    mainTemplate.Execute(w, nil)
+}
+ 
+func main() {
+    http.HandleFunc("/", mainPage)
+    http.ListenAndServe(":8080", nil)
+}
